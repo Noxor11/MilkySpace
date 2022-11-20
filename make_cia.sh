4377f356459de81df57b44546a7073211034ff8c
Brian Da Silva <brianjose2010@gmail.com>
2022-11-20 13:03:55 +0100

    Update make_cia.sh

--- make_cia.sh
+++ make_cia.sh
@@ -4,19 +4,28 @@ command -v bannertool >/dev/null 2>&1 || {
 }
 
 command -v bannertool >/dev/null 2>&1 || {
-        echo "makebanner not be found."
+        echo "makebanner could not be found."
         exit
 }
 
-if [[ `ls *.3dsx 2>/dev/null` != "SpaceGame.3dsx" ]]
-then
+command -v makerom >/dev/null 2>&1 || {
+        echo "makerom could not be found."
+        exit
+}
+
+
+count=`ls -1 *.3dsx 2>/dev/null | wc -l`
+if [ $count == 0 ]
+then 
     echo ".3dsx file not found. Did you compile the program with make?"
     exit
 fi
 
+game_name=`ls *.3dsx 2>/dev/null | cut -d. -f1`
 
-bannertool makebanner -i banner.png -a romfs/sound/confirm_selection.wav -o SpaceGame.bnr
 
-makerom -f cia -o SpaceGame.cia -rsf SpaceGame.rsf -elf SpaceGame.elf -exefslogo -icon SpaceGame.smdh -banner SpaceGame.bnr
+bannertool makebanner -i banner.png -a romfs/sound/confirm_selection.wav -o $game_name.bnr
 
-echo "Created SpaceGame.cia"
+makerom -f cia -o $game_name.cia -rsf $game_name.rsf -elf $game_name.elf -exefslogo -icon $game_name.smdh -banner $game_name.bnr
+
+echo "Created $game_name.cia"
